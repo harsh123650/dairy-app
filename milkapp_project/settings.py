35299ab92d8f8513ad218941ec360d28b2134dae
Harshal <harshal123@gmail.com>
2025-06-20 19:36:46 +0530

    Collect static files for deployment

--- milkapp_project/settings.py
+++ milkapp_project/settings.py
@@ -1,66 +1,21 @@
-
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'dummy-secret-key'
-DEBUG = True
-ALLOWED_HOSTS = ['dairy-app-56d5.onrender.com', '127.0.0.1', 'localhost']
+STATIC_URL = '/static/'
 
+# Where Django looks for your app static files (CSS/images/etc.)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dairyapp', 'static')]
 
+# Where Django collects all static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',  # ✅ THIS LINE
     'dairyapp',
 ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-]
-
-ROOT_URLCONF = 'milkapp_project.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'milkapp_project.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-AUTH_PASSWORD_VALIDATORS = []
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Kolkata'
-USE_I18N = True
-USE_TZ = True
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'dairyapp' / 'static']
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
